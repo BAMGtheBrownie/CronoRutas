8a6289e022b21e5d546304d13cae838f447a0ed9
Braulio <149034006+BAMGtheBrownie@users.noreply.github.com>
2026-09-16 14:40:42 -0600

    Update firebase.js

--- ver0.2.3 Cronorutas Chofer/firebase.js
+++ ver0.2.3 Cronorutas Chofer/firebase.js
@@ -3,14 +3,14 @@ import "firebase/auth";
 import "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAlZR4s8-03gQVLD21ZmHdUrCgj56JJLa8",
-  authDomain: "cronorutas-cec37.firebaseapp.com",
-  databaseURL: "https://cronorutas-cec37-default-rtdb.firebaseio.com",
-  projectId: "cronorutas-cec37",
-  storageBucket: "cronorutas-cec37.firebasestorage.app",
-  messagingSenderId: "783697441480",
-  appId: "1:783697441480:web:30617578e68bafe3186bfd",
-  measurementId: "G-N4JBHM98QL"
+  apiKey: " ",
+  authDomain: " ",
+  databaseURL: " ",
+  projectId: " ",
+  storageBucket: " ",
+  messagingSenderId: " ",
+  appId: " ",
+  measurementId: " "
 };
 
 if (!firebase.apps.length) {
